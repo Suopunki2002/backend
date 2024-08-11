1db60bb06a26397b0ec105d3a255be41b58cc1ae
Alex Suopanki <alex.suopanki@gmail.com>
2024-08-11 14:46:55 +0300

    uibuild

--- index.js
+++ index.js
@@ -15,48 +15,70 @@ app.use(morgan(
 ));
 
 app.get('/info', (request, response, next) => {
-  Person.countDocuments({}).then(count => {
-    const personCount = `Phonebook has info for ${count} people`;
-    const date = new Date().toUTCString();
-    response.send(`<p>${personCount}</p><p>${date}</p>`);
-  }).catch(error => next(error));
+  Person
+    .countDocuments({})
+    .then(count => {
+      const personCount = `Phonebook has info for ${count} people`;
+      const date = new Date().toUTCString();
+      response.send(`<p>${personCount}</p><p>${date}</p>`);
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body;
+
   if (!body.name) {
-    return response.status(400).json({
-      error: 'name missing'
-    });
+    return response.status(400).json({ error: 'name missing' });
   }
+
   if (!body.number) {
-    return response.status(400).json({
-      error: 'number missing'
-    });
+    return response.status(400).json({ error: 'number missing' });
   }
-  const person = new Person({
+
+  const person = {
     name: body.name,
     number: body.number
-  });
-  person.save().then(savedPerson => {
-    response.json(savedPerson);
-  }).catch(error => next(error));
+  };
+
+  Person
+    .findOneAndUpdate({ name: body.name }, person, { new: true, upsert: true })
+    .then(savedOrUpdatedPerson => response.json(savedOrUpdatedPerson))
+    .catch(error => next(error));
 });
 
 app.get('/api/persons', (request, response, next) => {
-  Person.find({}).then(persons => {
-    response.json(persons);
-  }).catch(error => next(error));
+  Person
+    .find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  }).catch(error => next(error));
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+  
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
